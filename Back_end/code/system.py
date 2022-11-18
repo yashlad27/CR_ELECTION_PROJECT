2203fc961865447371ec0e9dc48f9e7466d2e6b1
Yash Lad <yashlad727@gmail.com>
2022-11-18 08:42:01 +0530

    Student_table created with MySQL in XAMPP

--- Back_end/code/system.py
+++ Back_end/code/system.py
@@ -1,3 +1,8 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(host="localhost", user="root", password="")
+cnx = mysql.connector.connect(host="localhost", user="root", password=""
+                              ,database="cr_election")
+my_cursor = cnx.cursor()
+
+my_cursor.execute("CREATE TABLE student_table(srno INT, prn INT primary key, rollno VARCHAR(255), name VARCHAR(255), "
+                  "gender VARCHAR(10), has_voted INT, candidate_id INT DEFAULT NULL)")
