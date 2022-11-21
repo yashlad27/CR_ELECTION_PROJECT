e36e0f17b3591e2a10ac020f76b6b49ce78d1171
Yash Lad <71868797+yashlad27@users.noreply.github.com>
2022-11-21 12:27:59 +0530

    cardlist and cards for adding info of students

---
+++ Back_end/code/CardList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({robots}) => {
+
+    // const cardArray = robots.map((user,i) => {
+    //     return (<Card 
+    //     key={i} 
+    //     id = {robots[i].id} 
+    //     name={robots[i].name} 
+    //     email={robots[i].email} 
+    //     />
+    //     )
+    // })
+    // return (
+    //     {cardArray}
+    // );
+
+    return (
+        <div>
+          {
+            robots.map((user, i) => {
+              return (
+                <Card
+                  key={i}
+                  id={robots[i].id}
+                  name={robots[i].name}
+                  email={robots[i].email}
+                  />
+              );
+            })
+          }
+        </div>
+      );
+}
+
+export default CardList;
