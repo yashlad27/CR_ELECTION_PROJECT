e348b56cfbf043005218f9645237017021ac1795
Yash Lad <yashlad727@gmail.com>
2022-11-25 20:08:03 +0530

    props

--- Front_end/components/dbms-app/src/App.js
+++ Front_end/components/dbms-app/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-
-import card from "/Users/yash/Documents/GitHub/CR_ELECTION_PROJECT/Front_end/components/dbms-app/src/card.jsx";
+import card from "./card";
+import Card from "@material-u/core/Card";
 
 function App() {
     return(
         <div className="App">
-            
-            <card 
+            {/* <card 
             title = 'Card View'
             imageURL = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vvmpWt0qBu3LeBgZuUfmGAHaFt%26pid%3DApi&f=1&ipt=e932c9d4eaba93c80bba85b15848b93563500655fc76c1d2f1f2724939b6958b&ipo=images'
-            body = 'Lorem Ipsum'/>
+            body = 'Lorem Ipsum'/> */}
+
+            <card 
+            title='card view'
+            imageURIL = ""/>
+
+            <Card />
         </div>  
     );
 }
--- Front_end/components/dbms-app/src/card.js
+++ Front_end/components/dbms-app/src/card.js
@@ -1,25 +1,30 @@
 import React from "react";
 import './card.css';
 
-function card({title, imageURL, body}) {
+function card(props) {
     return (
         <div className="card-container">
             <div className="image-container">
-            <img src = {imageURL} alt = "image" />
+            <img src = {props.imageURL} alt=""/>
             </div>
+
             <div className="card-content">
             <div className="card-title">
-                <h3>{title}</h3>
+                <h3>{props.title}</h3>
             </div>
             <div className="card-body">
-                <p>{body}</p>
+                <p>{props.body}</p>
             </div>
             </div>
+
             <div className="btn">
                 <button>
                         VOTE ME!
                 </button>
             </div>
+
         </div>
     )
 }
+
+export default card;
