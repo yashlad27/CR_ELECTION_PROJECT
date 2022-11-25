0b7daee9375ae4ba07893115acf7bb5be1b1839c
Yash Lad <yashlad727@gmail.com>
2022-11-26 01:19:24 +0530

    adding html, css card designs

--- Front_end/components/dbms-app/src/App.js
+++ Front_end/components/dbms-app/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import card from "./card";
-import Card from "@material-u/core/Card";
+// import CardContent from "@material-ui/core/CardContent";
+// import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+// import CardActions from "@material-ui/core/CardActions";
 
 function App() {
     return(
@@ -10,11 +13,7 @@ function App() {
             imageURL = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vvmpWt0qBu3LeBgZuUfmGAHaFt%26pid%3DApi&f=1&ipt=e932c9d4eaba93c80bba85b15848b93563500655fc76c1d2f1f2724939b6958b&ipo=images'
             body = 'Lorem Ipsum'/> */}
 
-            <card 
-            title='card view'
-            imageURIL = ""/>
-
-            <Card />
+            <card />
         </div>  
     );
 }
