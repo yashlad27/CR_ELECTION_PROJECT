f95d44192b1000f8c1d4393fd56e38571cc3620c
Yash Lad <yashlad727@gmail.com>
2022-11-25 13:36:15 +0530

    student info.json file

---
+++ Front_end/code/student_info.js
@@ -0,0 +1,36 @@
+const stud_info = [
+    {
+        id: 1,
+        name: "Akshit Mahaur",
+        image: "https://avatars.githubusercontent.com/u/72341453?v=4",
+        alt: "Student Image",
+        year: "Third Year",
+        panel: "Panel-C",
+    },
+    {
+        id: 2,
+        name: "Akshit Mahaur",
+        image: "https://avatars.githubusercontent.com/u/72341453?v=4",
+        alt: "Student Image",
+        year: "Third Year",
+        panel: "Panel-C",
+    },
+    {
+        id: 3,
+        name: "Akshit Mahaur",
+        image: "https://avatars.githubusercontent.com/u/72341453?v=4",
+        alt: "Student Image",
+        year: "Third Year",
+        panel: "Panel-C",
+    },
+    {
+        id: 4,
+        name: "Akshit Mahaur",
+        image: "https://avatars.githubusercontent.com/u/72341453?v=4",
+        alt: "Student Image",
+        year: "Third Year",
+        panel: "Panel-C",
+    }
+];
+
+export default stud_info;
